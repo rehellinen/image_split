6b9d674a795a6f2675a08b4b28e675ac47a0574e
rehellinen <912377791@qq.com>
2018-11-13 22:43:58 +0800

    jimp获取图像的RGB信息

---
+++ server/utils/ProcessImage.js
@@ -0,0 +1,41 @@
+/**
+ *  ProcessImage.js
+ *  Create By rehellinen
+ *  Create On 2018/11/13 11:04
+ */
+const Jimp = require('jimp')
+
+export class ProcessImage {
+  constructor(image) {
+    this.image = image
+  }
+
+  async getRGB () {
+    const lenna = await Jimp.read(this.image)
+
+    console.log(lenna)
+
+    let red = []
+    let green = []
+    let blue = []
+    let count = 0
+    let index = 0
+
+    lenna.scan(0, 0, lenna.bitmap.width, lenna.bitmap.height, function (x, y, idx) {
+      if (count % lenna.bitmap.width === 0 && count >= lenna.bitmap.width) {
+        index ++
+      }
+
+      if (!red[index]) red[index] = []
+      if (!green[index]) green[index] = []
+      if (!blue[index]) blue[index] = []
+
+      red[index].push(this.bitmap.data[idx + 0])
+      green[index].push(this.bitmap.data[idx + 1])
+      blue[index].push(this.bitmap.data[idx + 2])
+      count ++
+    })
+
+    console.log(green[4].length)
+  }
+}
