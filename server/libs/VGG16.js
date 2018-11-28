cb93f0c56748053b1a26e1475a070fe07a71d575
rehellinen <912377791@qq.com>
2018-11-28 10:01:12 +0800

    神经网络定义完成

--- server/libs/VGG16.js
+++ server/libs/VGG16.js
@@ -4,31 +4,92 @@
  *  Create On 2018/11/26 10:55
  */
 import * as tf from '@tensorflow/tfjs'
+import {resolve} from 'path'
+
+const r = path => resolve(__dirname, path)
 
 export class VGG16 {
   constructor (pixels, width = 224, height = 224) {
-    this.clusters = pixels
+    this.pixels = pixels
     this.width = width
     this.height = height
   }
 
-  process () {
+  async process () {
+    const data = this.processData()
+    this.defineModel()
+    const res = await this.train(data)
+    return res
+  }
 
+  async train (data) {
+    // const res = await this.model.save(r('../model/'))
+    console.log(tf.tensor4d(data))
+    this.model.predict(tf.tensor4d(data))
+  }
+
+  processData () {
+    let newData = []
+    let yIndex = 0
+    for (let i = 0; i < this.pixels.length; i++) {
+      if (i % this.width === 0 && i >= this.width) {
+        yIndex++
+      }
+      if (!Array.isArray(newData[yIndex])) newData[yIndex] = []
+      newData[yIndex].push(this.pixels[i])
+    }
+    return newData
   }
 
   defineModel () {
     this.model = tf.sequential()
-    this.addConvolution(3, 64, 'relu', {
+    // 1
+    this.addConvolution(3, 64, {
       inputShape: [this.width, this.height, 3]
     })
-    this.addPooling()
+    this.addConvolution(3, 64)
+    this.addPooling(2, 2)
+    // 2
+    this.addConvolution(3, 128)
+    this.addConvolution(3, 128)
+    this.addPooling(2, 2)
+    // 3
+    this.addConvolution(3, 256)
+    this.addConvolution(3, 256)
+    this.addConvolution(3, 256)
+    this.addPooling(2, 2)
+    // 4
+    this.addConvolution(3, 512)
+    this.addConvolution(3, 512)
+    this.addConvolution(3, 512)
+    this.addPooling(2, 2)
+    // 5
+    this.addConvolution(3, 512)
+    this.addConvolution(3, 512)
+    this.addConvolution(3, 512)
+    this.addPooling(2, 2)
+
+    this.model.add(tf.layers.flatten())
+    this.addDense(4096)
+    this.model.add(tf.layers.dropout({ rate: 0.5 }))
+    this.addDense(4096)
+    this.model.add(tf.layers.dropout({ rate: 0.5 }))
+    this.addDense(1000, 'softmax')
   }
 
-  addConvolution (kernelSize, filters, activation = 'relu', otherConf) {
+  addDense (units, activation = 'relu') {
+    this.model.add(tf.layers.dense({
+      units,
+      activation
+    }))
+  }
+
+  addConvolution (kernelSize, filters, otherConf, activation = 'relu', padding = "same") {
     let config = Object.assign({}, otherConf, {
       kernelSize,
       filters,
-      activation
+      activation,
+      padding
     })
     this.model.add(tf.layers.conv2d(config))
   }
